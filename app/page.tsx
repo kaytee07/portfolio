210f6e15b6221a651df73889311f46e068a3fbd1
kaytee07 <kofi.taylor07@gmail.com>
2024-05-22 16:51:02 +0000

    hero page

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from '@/components/Hero'
+import React from 'react'
+
+const Home = () => {
+  return (
+      <main className="relative bg-black-100
+      flex justify-center items-center flex-]
+      overflow-hidden mx-auto sm:px-10 px-5">
+        <div className="max-w-7xl w-full">
+            <Hero/>
+        </div>
+      </main>
+  )
+}
+
+export default Home
+
