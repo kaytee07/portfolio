da259ec47d77af972191d8546463d6087f3de7ab
kaytee07 <kofi.taylor07@gmail.com>
2024-05-23 12:38:14 +0000

    Hero pag e completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect';
+import MagicButton from './ui/MagicButton';
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
+  const words = `Hey there, I'm Taylor. 
+  I transforming concepts into reality
+`;
   return (
     <div className="pb-20 pt-36">
         <div>
@@ -12,12 +18,35 @@ const Hero = () => {
             <Spotlight className="top-28 left-80 h-[80vh]
             w-[50vw]" fill="white"/>
         </div>
-        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+        <div className="h-screen w-full dark:bg-black-100
+         bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] 
+         absolute flex items-center justify-center">
           {/* Radial gradient for the container to give a faded look */}
           <div className="absolute pointer-events-none inset-0 
           flex items-center justify-center dark:bg-black-100 
           bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
          
+        </div>
+        <div className="flex justify-center relative my-20 z-10">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw]
+          flex flex-col items-center justify-center">
+            <h2 className="uppercase tracking-widest text-xs 
+            text-center text-blue-100 max-w-80">
+              Dynamic web Magic with Next.js
+            </h2>
+            
+             <TextGenerateEffect 
+              className="text-center text-[40px] md:text-5xl lg:text-6xl"
+              words={words} />
+
+              <MagicButton 
+              name="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+              handleClick={() => {}}
+              />
+              
+          </div>
         </div>
     </div>
   )
